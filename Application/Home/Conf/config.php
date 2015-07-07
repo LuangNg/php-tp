20a915fd48e66efcde25a393b06c9d44790bd1e1
luang <luangng@163.com>
2015-07-07 17:24:57 +0800

    add angular-route.js

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -4,4 +4,9 @@ return array(
 	'SESSION_AUTO_START'		=>			true,
 	'DEFAULT_V_LAYER' 			=> 			'View',
 	'DEFAULT_THEME'				=>			'Default',
+	'TMPL_PARSE_STRING'			=>			array(
+												// '../Public'	=>	MODULE_PATH.'View/Public/',
+												// '__TMPL__' 	=>	MODULE_PATH.'View/default/',
+												'__VIEW__'	=>	MODULE_PATH.'View/default',
+											)
 );
