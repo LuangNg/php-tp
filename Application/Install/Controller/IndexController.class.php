020a48038b3f7c23528a5e2b123a8c6803e0a878
luang <luangng@163.com>
2015-07-08 11:49:39 +0800

    modify install file

--- Application/Install/Controller/IndexController.class.php
+++ Application/Install/Controller/IndexController.class.php
@@ -1,8 +1,25 @@
 <?php
 namespace Install\Controller;
 use Think\Controller;
+use Think\Storage;
+
 class IndexController extends Controller {
     public function index(){
+        $this ->startInstall();
+        $this ->completeInstall();
+    }
+    
+    public function startInstall(){
+        if(Storage::has(__APP__.'/install.lock')){
+            $msg = 'You have already installed this program!';
+            $this ->error($msg);
+        }
         $this->display();
+    }
+    
+    public function completeInstall(){
+        //put lock file
+        Storage::put(__APP__.'/install.lock', 'lock');
+        $this->redirect();
     }
 }
---
+++ Application/Install/Controller/InstallController.class.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Install\Controller;
+
+use Think\Controller;
+
+class InstallController extends Controller {
+
+    public function step1() {
+        
+    }
+
+    public function step2() {
+        
+    }
+
+    public function step3() {
+        
+    }
+
+    public function step4() {
+        
+    }
+
+}
