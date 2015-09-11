6518f61c8f0d11f9834a97cd19db47af3d07fce2
luang <luangng@163.com>
2015-09-11 10:35:49 +0800

    modi indexcontroller

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -8,14 +8,6 @@ use Think\Storage;
 class IndexController extends Controller {
 
     public function index() {
-        //if this application hasn't been installed, redirect ro installing page
-        if (!Storage::has($_SERVER['DOCUMENT_ROOT'] . __ROOT__ . '/install.lock')) {
-            header('Location:' . __ROOT__ . '/install.php');
-        }
-        $this->display();
-    }
-
-    public function detail() {
         $this->display();
     }
 
